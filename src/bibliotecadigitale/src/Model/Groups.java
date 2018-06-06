099bdad66459e218ccc4a220aa0e2b201d8b05d7
Fabgen5 <37864192+Fabgen5@users.noreply.github.com>
2018-06-06 16:46:25 +0200

    v.23

---
+++ src/bibliotecadigitale/src/Model/Groups.java
@@ -0,0 +1,8 @@
+package Model;
+/**
+ * @author Fabio
+ */
+public class Groups {
+
+	private String nome;
+}
---
+++ src/bibliotecadigitale/src/Model/Utente.java
@@ -0,0 +1,73 @@
+package Model;
+import java.sql.Date;
+/**
+ * @author Fabio
+ */
+public class Utente {
+
+	private String usermail;
+	private String nome;
+	private String cognome;
+	private String citta;
+	private Date datanascita;
+	private int gruppo;
+
+	    public Utente(String usermail, String nome, String cognome, String citta, Date datanascita, int gruppi) {
+	        this.usermail = usermail;
+	        this.nome = nome;
+	        this.cognome = cognome;
+	        this.citta = citta;
+	        this.datanascita = datanascita;
+	        this.gruppo= gruppi;
+	    }
+
+	    public String getUsermail() {
+	        return usermail;
+	    }
+
+	    public void setUsermail(String usermail) {
+	        this.usermail = usermail;
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public String getCognome() {
+	        return cognome;
+	    }
+
+	    public void setCognome(String cognome) {
+	        this.cognome = cognome;
+	    }
+
+	    public String getCitta() {
+	        return citta;
+	    }
+
+	    public void setCitta(String citta) {
+	        this.citta = citta;
+	    }
+
+	    public Date getDatanascita() {
+	        return datanascita;
+	    }
+
+	    public void setDatanascita(Date datanascita) {
+	        this.datanascita = datanascita;
+	    }
+
+	    public int getGruppo() {
+	        return gruppo;
+	    }
+
+	    public void setGruppo(int gruppo) {
+	        this.gruppo = gruppo;
+	    }
+	    
+	    
+	}
